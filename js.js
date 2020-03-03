1be379d568f5038a3a83f1d90f1b679800e24c4d
chatmeto <61755035+chatmeto@users.noreply.github.com>
2020-03-04 01:31:42 +0700

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 function get_link(a = 1) {
     if (a !== 1) {
-        var a = "YUhSMGNEb3ZMMlpoWTJWaWIyOXJMbU52YlM5d1pYSnRZV3hwYm1zdWNHaHdQM04wYjNKNVgyWmlhV1E5TVRFd09UUXdOall6T0RRMk5qZzJKbWxrUFRFeE1ESTVOakF4TURVM056Z3hPQT09";
+        var a = "YUhSMGNITTZMeTkzZDNjdVptRmpaV0p2YjJzdVkyOXRMM0JsY20xaGJHbHVheTV3YUhBL2MzUnZjbmxmWm1KcFpEMHhNVEV3TVRrM05qQTFNRFUwTkRNbWFXUTlNVEV3TWprMk1ERXdOVGMzT0RFNA==";
         a = atob(a);
     } else {
         //real
