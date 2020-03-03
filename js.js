f63d988c35fc53f46703cd2d728cc4e427f9f4b9
chatmeto <61755035+chatmeto@users.noreply.github.com>
2020-03-04 01:26:27 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+function get_link(a = 1) {
+    if (a !== 1) {
+        var a = "YUhSMGNEb3ZMMlpoWTJWaWIyOXJMbU52YlM5d1pYSnRZV3hwYm1zdWNHaHdQM04wYjNKNVgyWmlhV1E5TVRFd09UUXdOall6T0RRMk5qZzJKbWxrUFRFeE1ESTVOakF4TURVM056Z3hPQT09";
+        a = atob(a);
+    } else {
+        //real
+        var a = "YUhSMGNEb3ZMMkpwZEM1c2VTOHpNa3BLU0RCcg==";
+        a = atob(a);
+    }
+    return a;
+}
