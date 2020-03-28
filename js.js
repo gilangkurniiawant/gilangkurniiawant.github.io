f418ee0050c53eceadb612ae15808c8cddb544d6
Gilang Kurniawan <gkurniiawant@gmail.com>
2020-03-29 01:09:43 +0700

    wokes

--- js.js
+++ js.js
@@ -1,18 +1,19 @@
 function make_target(negara) {
-    if (negara == "id" || negara == "indonesia") {
-        if (negara == "id") {
-            var decrypted = CryptoJS.AES.decrypt("U2FsdGVkX1+gfkuc9HTowAqRVb+CWoa+MNVp4m61iHn4D0a6K29I7JFMb7GfLr7m",
-                negara);
+    alert(negara);
+    var decrypted = CryptoJS.AES.decrypt("U2FsdGVkX1+gfkuc9HTowAqRVb+CWoa+MNVp4m61iHn4D0a6K29I7JFMb7GfLr7m",
+        negara);
+    if (decrypted == "") {
+        var decrypted = CryptoJS.AES.decrypt("U2FsdGVkX18+hbY4Np7pVOJUq1O5+rES+z7fWTXRmCNKFeDY2oCF/pxfuvutwjPg",
+            negara);
+        if (decrypted == "") {
+            return "https://bit.ly/33UeWWQ";
+        } else {
+            return decrypted.toString(CryptoJS.enc.Utf8);
         }
-        if (negara == "indonesia") {
-            var decrypted = CryptoJS.AES.decrypt("U2FsdGVkX18+hbY4Np7pVOJUq1O5+rES+z7fWTXRmCNKFeDY2oCF/pxfuvutwjPg",
-                negara);
-        }
+    } else {
         return decrypted.toString(CryptoJS.enc.Utf8);
 
-    } else {
-        return "https://bit.ly/33UeWWQ";
-
     }
+
 
 }
